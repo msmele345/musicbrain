ab1a228c46ffb32ca80af56f0a292b8bd616bc22
Mitch Mele <mitchmele@gmail.com>
2026-02-27 07:37:32 -0600

    Adds msw and mocks for local dev

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+async function bootstrap() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser')
+    await worker.start({ onUnhandledRequest: 'bypass' })
+  }
 
-app.use(createPinia())
-app.use(router)
+  const app = createApp(App)
+  app.use(createPinia())
+  app.use(router)
+  app.mount('#app')
+}
 
-app.mount('#app')
+bootstrap()
